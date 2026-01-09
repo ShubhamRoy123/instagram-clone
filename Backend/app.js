ea4c6a42feb591a19aa97ffe5f35b03229ab921e
ShubhamRoy123 <roy388925@gmail.com>
2026-01-09 21:40:03 +0530

    making user model

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,8 @@
 const express=require('express');
 const app=express();
 const port=3000;
-
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 app.get('/',(req,res) => {
 	res.send("hello world");
@@ -11,3 +12,7 @@ app.get('/',(req,res) => {
 app.listen(port,() =>{
 	console.log(`Example app listen on port ${port}`)
 })
+
+mongoose.connect('')
+	.then(() => console.log("Connected to MongoDB"))
+	.catch(err => console.error("Could not connect to MongoDB",err));
---
+++ Backend/src/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	username : {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	profilePic: {
+		type: String,
+		default: "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
+	},
+	bio: {
+		type: String,
+		maxlength: 150,
+		default: ""
+	},
+
+	followers: [{
+		type: mongoose.Schema.Types.Objectid,
+		ref: 'User'
+	}]
+},{ timestamps: true});
+
+module.exports = mongoose.model('User', userSchema);
