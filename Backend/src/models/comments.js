5364719c2b3ae536f75b0ba59cfbbe208cbe0c11
ShubhamRoy123 <roy388925@gmail.com>
2026-01-10 21:49:50 +0530

    adding comment model

--- Backend/src/models/comments.js
+++ Backend/src/models/comments.js
@@ -1,1 +1,36 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+	// user who comment
+	postId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Post',
+		required: true
+	},
+	// the user who Comment
+	author: { 
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	text: {
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 1000
+	},
+	// Array of Users who liked this specifis comment
+	likes: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}],
+	parentComment: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Comment',
+		default: null
+	}
+});
+
+commentSchema.index({ postId: 1, createdAt: -1 });
+
+module.exports = mongoose.model('Comment',commentSchema);
