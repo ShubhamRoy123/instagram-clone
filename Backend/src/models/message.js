41754bc2045c945db1d8ab21cdfe1f66994c5aa7
ShubhamRoy123 <roy388925@gmail.com>
2026-01-10 21:17:11 +0530

    adding message model

--- Backend/src/models/message.js
+++ Backend/src/models/message.js
@@ -1,1 +1,32 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+	conversationId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Conversation',
+		required: true
+	},
+	sender: {
+		type: mngoose.Schema.Types.Objectid,
+		ref: "User",
+		required: true
+	},
+	text: {
+		type: String,
+		required: true'
+		trim: true
+	},
+	// for sending images 
+	image: {
+		type: String,
+		default: null
+	},
+	seen: {
+		type: Boolean,
+		default: false
+	}
+}, {
+	timestamp: true
+});
+
+module.exports = mongoose.model('Message', messageSchema);
